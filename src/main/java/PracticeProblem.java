d7cb815197b695c27d38948f49571e90fa20c356
wayne-bai-ics4u1-02-25 <wayne.bai@student.tdsb.on.ca>
2025-03-04 15:20:00 +0000

    problems with test cases? (Replaced "Integer" with "int" when declaring integer array, removed an unneeded ";" token

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -24,4 +24,44 @@ public class PracticeProblem {
 		//Write question 5 code here
 	}
 
+	public static int sum2D(int[][] arr) {
+		int sum = 0;
+		for (int x = 0; x < arr.length; x++) {
+			for (int y = 0; y < arr[x].length; y++) {
+				sum += arr[x][y];
+			}
+		}
+		return sum;
+	}
+
+	public static int sumRow(int[][] arr, int r) {
+		int sum = 0;
+		for (int y = 0; y < arr[r].length; y++) {
+			sum += arr[r][y];
+		}
+		return sum;
+	}
+
+	public static int sumColumn(int[][] arr, int c) {
+		int sum = 0;
+		for (int x = 0; x < arr.length; x++) {
+			sum += arr[x][c];
+		}
+		return sum;
+	}
 }
+
+/*
+[![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=18520748)
+# Instructions  
+
+  ## Questions
+  1. Complete the _sum2D_ method by writing a method that calculates the sum of all the element in the 2D integer array passed as a parameter. Return the sum.
+
+2. Complete the _sumRow_ method. The method should take in an 2D integer array and a row number. The method should return the sum of the elements in a given row.</br>
+For example, if row = 0, you should return the sum of all the elements in the topmost row.
+
+3. Complete the _sumColumn_ method so that given a 2-D int array and an integer representing the column to sum, sum up the all elements in that column in the given array.
+
+
+*/
--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -145,7 +145,7 @@ public class PracticeProblemTest {
          //      { 2, 3, -10, 6 } };
          assertEquals(-15, (int) method.invoke(null, new int[][]{ { 1, 3, -4, 6 },
                { 3, -34, 7, 2 },
-               { 2, 3, -10, 6 } };));
+               { 2, 3, -10, 6 } }));
       }
 
       catch (NoSuchMethodException e) {
@@ -163,7 +163,7 @@ public class PracticeProblemTest {
          Class[] cArg = { int[][].class };
          Method method = testClass.getDeclaredMethod("sum2D", cArg);
          // Enter code here
-         Integer[][] arr = { { 0, 0, 0, 0 },
+         int[][] arr = { { 0, 0, 0, 0 },
                { 0, 0, 0, 0 },
                { 0, 0, 0, 0 } };
          assertEquals(0, (int) method.invoke(null, arr));
@@ -184,7 +184,7 @@ public class PracticeProblemTest {
          Class[] cArg = { int[][].class };
          Method method = testClass.getDeclaredMethod("sum2D", cArg);
          // Enter code here
-         Integer[][] arr = { { 1, 3, 4, 6 },
+         int[][] arr = { { 1, 3, 4, 6 },
                { 3, 5, 7, 2 },
                { 2, 3, 5, 6 } };
          assertEquals(47, (int) method.invoke(null, arr));
